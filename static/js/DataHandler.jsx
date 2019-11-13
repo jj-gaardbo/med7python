39f3406e333bfc6ee6566bb23c5b8f9fb83dfe62
jj-gaardbo <jgaard16@gmail.com>
2019-11-13 09:53:50 +0100

    Added icon

--- static/js/DataHandler.jsx
+++ static/js/DataHandler.jsx
@@ -16,7 +16,7 @@ export default class DataHandler extends React.Component {
             x: 0,
             percentage: 0,
             intervalID: 0,
-            timeout: 5,
+            timeout: 30,
             skip_frames: 1,
             meta: null,
             period_pos: "",
